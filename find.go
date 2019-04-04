1e8649fddab5843d940701e9b130a88685e0a03f
John Schroeder <john@schroederspace.com>
2019-04-04 19:39:59 -0400

    basic walking

--- find.go
+++ find.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func find(root string) []string {
+	var ret []string
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			fmt.Printf("failure accessing a path %q: %v\n", path, err)
+			return err
+		}
+		if !strings.HasPrefix(path, root) {
+			path = root + path
+		}
+		ret = append(ret, path)
+		return nil
+	})
+	if err != nil {
+		fmt.Printf("error walking the path %q: %v\n", root, err)
+		return ret
+	}
+	return ret
+}
 
 func main() {
-	fmt.Println("Hello World")
+	output := find("./")
+	for _, entry := range output {
+		fmt.Println(entry)
+	}
 }
